aa8deef65e588c887abe29e86bdc68945309c804
Andrew Krusniak <andrew.krusniakjr@gmail.com>
2022-12-21 07:40:05 -0500

    forward function

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -44,9 +44,9 @@ recvThread.start()
 # CREATE FUNCTIONS HERE....
 
 
-print("\nFirst & Last Names")
-print("Program Name: ")
-print("Date: ")
+print("\n Andrew KRUSNIAK")
+print("Program Name: DRONES ")
+print("Date:12/20/22 ")
 print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
 ready = input('\nAre you ready to take flight: ')
@@ -58,9 +58,7 @@ try:
 
         sendmsg('command', 0)
         sendmsg('takeoff')
-
-        # Review the (SDK) Software Development Kit resource for Drone Commands
-        # Delete these comments before writing your program
+        sendmsg('forward', 100)
 
         sendmsg('land')
 
